f9ffdf24adfd1946af2bac492318980c2427ec27
Adarsh Gayadolli <137860068+Adarshpg@users.noreply.github.com>
2026-02-04 13:12:16 +0000

    datatype convertion

---
+++ 4.datatype_conversion/04_dc.js
@@ -0,0 +1,28 @@
+// let score=null
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber=Number(score)
+// console.log(typeof(valueInNumber));
+// console.log(valueInNumber);
+
+//"33" => 33
+//"33abc" => NaN(Not a Number)
+//true => 1; false =>0
+
+// let isLoggedIn="Adarsh"
+// let booleanisLoggedIn=Boolean(isLoggedIn)
+// console.log(isLoggedIn);
+// console.log(booleanisLoggedIn); 
+// console.log(typeof isLoggedIn);
+// console.log(typeof booleanisLoggedIn); 
+
+//1 => true; 0=>false
+//"" =>false
+//"Adarsh" => true
+
+let someNumber=33
+let StringNumber=String(someNumber)
+console.log(StringNumber)
+console.log(typeof StringNumber)
