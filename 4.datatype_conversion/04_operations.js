4deb2594b8d855d15fc69bf773e17a5b1f8ca52c
Adarsh Gayadolli <137860068+Adarshpg@users.noreply.github.com>
2026-02-05 05:12:56 +0000

    operations

---
+++ 4.datatype_conversion/04_operations.js
@@ -0,0 +1,40 @@
+let value = 3
+let negvalue = -value
+// console.log(negvalue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(3*2);
+// console.log(4**2);
+// console.log(4/2);
+
+let str1="hello"
+let str2=" Adarsh"
+let str3=str1+str2
+// console.log(str3)
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+// console.log((3+4)*5%3);
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+num1=num2=num3=2+2
+console.log(num1)
+
+//example for using increment operator before and after the increment
+let x = 3;
+const y = x++;
+
+console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3"
+
+let a = 3;
+const b = ++a;
+
+console.log(`a:${a}, b:${b}`);
+// Expected output: "a:4, b:4"
