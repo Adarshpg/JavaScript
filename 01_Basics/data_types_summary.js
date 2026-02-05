492146c670efcb46af80b6f12fa237f4a7f3940a
Adarsh Gayadolli <137860068+Adarshpg@users.noreply.github.com>
2026-02-05 10:27:18 +0000

    datatypesummary

---
+++ 01_Basics/data_types_summary.js
@@ -0,0 +1,39 @@
+//primitive
+
+// 7 types : String, Number, Boolean, null, undefined, Symbol, Bigint
+
+// const score=100
+// console.log(typeof score);
+// const score1=100.433
+// console.log(typeof score1);
+
+// let isLoggedIn=true
+// var isLoggedIn1=null
+// let userEmail;
+
+// console.table([isLoggedIn,userEmail])
+
+// const Id=Symbol("123")
+// const anotherId=Symbol("123")
+
+// console.log(Id===anotherId)
+
+// const bigNumber=3434535324342n
+// console.log(typeof bigNumber)
+
+
+//Reference(Non Primitive)
+
+//Array, Object, Functions
+
+const heros=["Adarsh","Ganesh","Harish"]
+let mydet={
+    name:"Adarsh",
+    age:22
+}
+
+const myFunction=function(){
+    console.log("Hello,World!")
+}
+
+console.log(typeof myFunction)
